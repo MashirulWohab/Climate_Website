d8df487444842bfe220c100c997d2ad901befd27
Mashirul Wohab <wohabmashirul@gmail.com>
2024-08-31 23:48:57 +0530

    Navbar Implemented

--- climate-snapshot/src/App.js
+++ climate-snapshot/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/tools'>Tools</Route>
+          <Route path='/data'>Data</Route>
+          <Route path='/help'>Help</Route>
+          <Route path='/blog'>Blog</Route>
+          <Route path='/events'>Events</Route>
+          <Route path='/about'>About</Route>
+          <Route path='*'>Error 404! Page Not Found</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
